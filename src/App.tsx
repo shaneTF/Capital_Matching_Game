2839d8f6cbeac2b158c2bd293568e791ea5fbed9
Shane Franklin <shanefranklin148@gmail.com>
2023-09-28 13:16:57 -0400

    Initial button layout with randomize bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 
+type ButtonState = "DEFAULT" | "SELECTED" | "WRONG";
+
+function CountryCapitalGame({ data }: { data: Record<string, string> }) {
+  const countries = Object.keys(data);
+  const capitals = Object.values(data);
+  const options = [...countries, ...capitals];
+  const [colorMap, setColorMap] = useState<Record<string, ButtonState>>({});
+
+  options.sort(() => Math.random() - 0.5);
+
+  return (
+    <>
+      {options.map((option) => (
+        <button
+          className={colorMap[option] === "SELECTED" ? "selected" : ""}
+          onClick={() => {
+            // set bg to blue 4009Bff
+            setColorMap({
+              ...colorMap,
+              [option]: "SELECTED",
+            });
+          }}
+        >
+          {option}
+        </button>
+      ))}
+    </>
+  );
+}
+
 function App() {
-  return <div>This is a test!</div>;
+  return (
+    <>
+      <CountryCapitalGame data={{ Germany: "Berlin", Azerbaijan: "Baku" }} />
+    </>
+  );
 }
 
 export default App;
